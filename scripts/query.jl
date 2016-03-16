b23e9fa3de97edb3a160396718bc103563a0f2c3
WooKyoung Noh <wookay.noh@gmail.com>
2016-03-16 18:09:38 +0900

    query examples

--- scripts/query.jl
+++ scripts/query.jl
@@ -4,3 +4,14 @@ simulator = App("http://localhost:8080")
 vc = initial(simulator)
 
 @query vc.view.backgroundColor = UIColor.greenColor()
+
+@query vc.view.alpha = 0.5
+@query vc.view.alpha = 1
+
+@query vc.label.text = "hello"
+@query vc.label.backgroundColor = UIColor.yellowColor()
+
+@query vc.label.frame = "{{10, 150}, {300, 200}}"
+@query vc.label.frame = CGRectMake(10, 250, 300, 200)
+
+@query vc.label.font = UIFont(name: "Helvetica", size: 30)
