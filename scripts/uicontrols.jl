9288e8fa605bfb229e871f47d6861460a0dc7712
WooKyoung Noh <wookay.noh@gmail.com>
2016-05-02 04:20:31 +0900

    update uicontrols

--- scripts/uicontrols.jl
+++ scripts/uicontrols.jl
@@ -4,30 +4,27 @@
 using Swifter
 
 app = initial("http://localhost:8080")
-@query vc = app.window.rootViewController
+vc = @query app.window.rootViewController
 
 @query vc.label.text = "Hello Swift"
+sleep(0.5)
 
 @query vc.textField.text = "Hello Julia"
+sleep(0.5)
 
 @query vc.button.tap()
-
 sleep(0.5)
 
 @query vc.switch_.tap()
-
-sleep(0.5)
-
-@query vc.segmentedControl.tap(index: 2)
-
 sleep(0.5)
 
 @query vc.segmentedControl.tap(title: "Second")
+sleep(0.5)
 
+@query vc.segmentedControl.tap(index: 2)
 sleep(0.5)
 
 @query vc.slider.tap(value: 1)
-
 sleep(0.5)
 
-@query vc.progressView.progress = 1
+@query vc.progressView.setProgress(1, animated: true)
