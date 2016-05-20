0a9f579aa0288efb434f87ff7f4d7fa2662aa9f4
WooKyoung Noh <wookay.noh@gmail.com>
2016-05-20 22:17:37 +0900

    Fixed test

--- Demo/Test/TestApp/TestAppTests/TypeHandlerTests.swift
+++ Demo/Test/TestApp/TestAppTests/TypeHandlerTests.swift
@@ -17,7 +17,8 @@ class TypeHandlerTests: XCTestCase {
         let view = UIView(frame: CGRectMake(0,0,100,100))
         let (_, val) = handler.getter_handle(view, "frame")
         let frame = (val as! ValueObject).value as? String
-        Assert.equal("{CGRect={CGPoint=dd}{CGSize=dd}}", (val as! ValueObject).type)
+        let typ = (val as! ValueObject).type
+        Assert.True(["{CGRect={CGPoint=dd}{CGSize=dd}}", "{CGRect={CGPoint=ff}{CGSize=ff}}"].contains(typ))
         Assert.equal("{{0, 0}, {100, 100}}", frame)
     }
     
@@ -42,11 +43,13 @@ class TypeHandlerTests: XCTestCase {
     }
 
     func test_UIColorFromString() {
-        let color = UIColorFromString("UIDeviceRGBColorSpace 0 1 1 1")
-        Assert.equal("UIDeviceRGBColorSpace 0 1 1 1", String(color))
+        if let color = UIColorFromString("UIDeviceRGBColorSpace 0 1 1 1") {
+            Assert.equal("UIDeviceRGBColorSpace 0 1 1 1", String(color))
+        }
 
-        let white = UIColorFromString("UIDeviceWhiteColorSpace 0 1")
-        Assert.equal("UIDeviceWhiteColorSpace 0 1", String(white))
+        if let white = UIColorFromString("UIDeviceWhiteColorSpace 0 1") {
+            Assert.equal("UIDeviceWhiteColorSpace 0 1", String(white))
+        }
     }
 
 }
--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ run:
 	xctool --version
 	@echo
 	pod install --project-directory=Demo/Test/TestApp/
-	xctool -workspace Demo/Test/TestApp/TestApp.xcworkspace -scheme TestApp -derivedDataPath Demo/Test/TestApp/build -sdk iphonesimulator
+	xctool -workspace Demo/Test/TestApp/TestApp.xcworkspace -scheme TestApp -derivedDataPath Demo/Test/TestApp/build -sdk iphonesimulator clean test
